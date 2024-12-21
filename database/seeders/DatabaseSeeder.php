0c97ee0b6d1d45307eb5a00c15208fe0864c6a96
alytamam <alytmaam@gmail.com>
2024-12-21 21:17:06 +0700

    updatex

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PositionSeeder::class,
             EmployeeSeeder::class,
-            // UsersSeeder::class
+            UsersSeeder::class
         ]);
     }
 }
